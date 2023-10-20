e20615dd1d2a734e65e43cc8bf8d58c52ab26731
LucasSousa353 <119136103+LucasSousa353@users.noreply.github.com>
2023-10-20 00:21:22 +0000

    Initial commit

---
+++ bundle.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function App() {
+  const institutoNome = 'O Instituto Federal de Educação, Ciência e Tecnologia de São Paulo';
+  const institutoEndereco = 'Localizado em Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010';
+
+  return (
+    <div>
+      <p>{institutoNome} - é uma autarquia federal de ensino.</p>
+      <p>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.</p>
+      <p>Durante sua história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.</p>
+      <p>Com a mudança, {institutoNome} passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.</p>
+      <p>“Além dos cursos presenciais, o {institutoNom  e} oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).</p>
+      <p>O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo;</p>
+      <p>{institutoEndereco}</p>
+    </div>
+  );
+}
+
+export default App;
